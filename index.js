c8b4312a2ede853aa15418fb8dc351563a3a4f39
Andy Niccolai <aniccolai@pivotallabs.com>
2014-03-21 17:54:30 -0600

    non-blocking file operations

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var jstransform            = require('jstransform')
   , visitUnderscoreOOStyle = require('./visitors/visit_underscore_oo_style').visitorList
-  , fs                     = require('fs');
+  , fs                     = require('fs')
+  , glob                   = require('glob');
 
-var fileName = process.argv[2],
-    transformedFileData = jstransform.transform(visitUnderscoreOOStyle, fs.readFileSync(fileName, 'utf-8')).code;
+var name = process.argv[2];
 
-fs.writeFileSync(fileName, transformedFileData);
+if (fs.lstatSync(name).isDirectory()) {
+  glob(name + "/**/*.js", {}, function (er, files) {
+    files.forEach(transformFile);
+  });
+} else {
+  transformFile(name);
+}
+
+function transformFile(fileName) {
+  fs.readFile(fileName, 'utf-8', function(err, data) {
+    if (err) return;
+    fs.writeFile(fileName, jstransform.transform(visitUnderscoreOOStyle, data).code);
+  });
+}
